7e04b7d459420f7ab7ac08cc2e10f055fd699d38
Drm Dev <cyberaioff@gmail.com>
2021-10-20 12:58:08 +0700

    Update update.js

--- commands/developer/update.js
+++ commands/developer/update.js
@@ -1,8 +1,54 @@
 let { execSync } = require('child_process')
+const { MessageActionRow, MessageButton } = require('discord.js')
+const Discord = require('discord.js')
+const invite = new MessageActionRow()
+    .addComponents(
+        new MessageButton()
+        .setLabel('YES')
+        .setCustomId('yes')
+        .setStyle('DANGER')
+    )
+    .addComponents(
+        new MessageButton()
+        .setLabel('NO')
+        .setCustomId('no')
+        .setStyle('SUCCESS')
+    )
+const standar = new Discord.MessageEmbed()
+    .setFooter("DotBot")
+    .setColor("YELLOW")
+    .setDescription("**Confirm Update**")
+    .setTimestamp()
+const already = new Discord.MessageEmbed()
+    .setFooter("DotBot")
+    .setColor("GREEN")
+    .setDescription("**Already up to date**")
+    .setTimestamp()
 exports.run = async(client, message, args) => {
-    let stdout = execSync('git remote set-url origin https://github.com/DemuraAIdev/DrmPR.git && git pull')
-    client.reload()
-    message.channel.send("```" + stdout.toString() + "```")
+    message.channel.send("Checking Update...")
+    message.channel.send({ embeds: [standar], components: [invite] });
+    const filter = i => i.user.id === client.config.bot.owner;
+    const collector = message.channel.createMessageComponentCollector({ filter, max: 1 });
+    collector.on('collect', async i => {
+        if (i.customId === 'yes') {
+            let stdout = execSync('git remote set-url origin https://github.com/DemuraAIdev/DrmPR.git && git pull')
+            const update = new Discord.MessageEmbed()
+                .setFooter("DotBot")
+                .setColor("GREEN")
+                .setDescription("Update complete\n```" + stdout + "```")
+                .setTimestamp()
+            if (stdout === "Already up to date.") {
+                await i.update({ embeds: [already], components: [] })
+                client.reload()
+            } else {
+                await i.update({ embeds: [update], components: [] })
+            }
+
+        }
+        if (i.customId === 'no') {
+            await i.update({ content: "canceled", embeds: [], components: [] })
+        }
+    });
 }
 
 exports.help = {
